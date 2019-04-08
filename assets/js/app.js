8199cda99c3ff221988c32551bde196216f65d04
KevinL <mastahkevin@gmail.com>
2019-04-08 09:00:47 -0700

    Updating JS to add functions for accessing DB. Also added styling and sematics to CSS & HTML.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,33 @@
-  // Initialize Firebase
-  const config = {
+// Initialize Firebase
+const config = {
     apiKey: "AIzaSyBYnc8j8pYiDuD3OPuvYVXDqImcVEQ_WSo",
     authDomain: "train-schedule-d3d15.firebaseapp.com",
     databaseURL: "https://train-schedule-d3d15.firebaseio.com",
     projectId: "train-schedule-d3d15",
     storageBucket: "train-schedule-d3d15.appspot.com",
     messagingSenderId: "154322965757"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  const db = firebase.database();
+const db = firebase.database();
 
-  let trainName = "";
-  let destination = "";
-  let startTime = "";
-  let frequency = 0;
+let trainName = "";
+let destination = "";
+let startTime = "";
+let frequency = 0;
+
+function currentTime() {
+    let current = moment().format('LT');
+    $("#currentTime").html(current);
+    setTimeout(currentTime, 1000);
+};
+
+// Need function to pull out values from input fields
+
+
+// Need function to save in db
+
+
+// Need function for submit button
+
+
